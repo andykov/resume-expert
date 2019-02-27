58005da686951bf6659fe65960ab26564ef92703
Andrey Budnikov <andykov.work@gmail.com>
2019-02-28 00:49:59 +0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,49 @@
 // import * as navigation from "./modules/navigation.js";
 // import * as fullscreen from "./modules/fullscreen.js";
 // import * as landspace from "./modules/landspace.js";
-// navigation.init();
+// import * as anim from "./modules/animation.js";
+// anim.init();
 // fullscreen.init();
 // landspace.init();
 
 $(function() {
+  
 
   // accordion faq
   $('.js-faq').on('click', function(e) {
-    var $this = $(this);
+      var $this = $(this);
 
-    if ($('.faq_answer').is(':visible')) {
-      $('.faq_answer').slideUp(150);
-    }
-    
-    if ($this.next().is(':visible')) {
-        $this.next().slideUp(200);
-    } else {
-        $this.next().slideDown(200);
-    }
-    
+      if ($('.faq_answer').is(':visible')) {
+        $('.faq_answer').slideUp(150);
+      }
+      
+      if ($this.next().is(':visible')) {
+          $this.next().slideUp(200);
+      } else {
+          $this.next().slideDown(200);
+      }
+      
+  });
+
+  // experts slider
+  $('.js-experts').slick({
+    dots: true,
+    arrows: false,
+    infinite: false,
+    autoplay: true,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
 });
 
-});
